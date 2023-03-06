d4495bef64ed8d92e3cd086210b5a9c6c871fc2f
Luciana2202 <luciana.santos2202@gmail.com>
2023-03-06 11:54:08 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,17 +12,53 @@ public class Main {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("curso java");
-        curso2.setDescricao("descrição curso java");
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
+
+        Conteudo conteudo = new Curso();
+        Conteudo conteudo1 = new Mentoria();
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso2);
+
+
+        /*System.out.println(curso2);
         System.out.println(curso1);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamo Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer.");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLuciana = new Dev();
+        devLuciana.setNome("Luciana");
+        devLuciana.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Luciana" + devLuciana.getConteudosInscritos());
+        devLuciana.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Luciana" + devLuciana.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Luciana" + devLuciana.getConteudosConcluidos());
+        System.out.println("XP:" + devLuciana.calcularTotalXp());
+
+        System.out.println("---------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos João" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" +devJoao.calcularTotalXp());
     }
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,38 +2,17 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    @Override
-    public String toString() {
-        return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", data=" + data +
-                '}';
-    }
+public class Mentoria extends Conteudo {
 
-    private String titulo;
-   private  String descricao;
     private LocalDate data;
+
+    @Override
+    public double calcularxp() {
+        return XP_PADRAO + 20d;
+    }
 
     public Mentoria() {
 
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public LocalDate getData() {
@@ -43,4 +22,14 @@ public class Mentoria {
     public void setData(LocalDate data) {
         this.data = data;
     }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                "data=" + data +
+                '}';
+    }
+
 }
